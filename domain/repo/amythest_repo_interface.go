6e190f47642db19ca34f4b7bba8f1f1ed59445b9
Kevin Christian Chandra <glovenkevincch@gmail.com>
2022-12-10 15:53:43 +0700

    feat(amythest): add generate circle API

--- domain/repo/amythest_repo_interface.go
+++ domain/repo/amythest_repo_interface.go
@@ -4,4 +4,5 @@ import "context"
 
 type AmythestRepoInterface interface {
 	GenerateWanted(ctx context.Context, imageLink string) ([]byte, error)
+	GenerateCircle(ctx context.Context, imageLink string) ([]byte, error)
 }
